b1b12e11087c977595ef92090ed38d8014fcb18d
Adi-Sose <Adi.Sose@Outlook.com>
2019-09-08 01:22:05 +0200

    Motor class moved to header file

---
+++ ArduinoApp/lib/Motor/Motor.cpp
@@ -0,0 +1,27 @@
+#include <Motor.h>
+#include <Arduino.h>
+
+Motor::Motor(int stepPin, int dirPin)
+{
+  this->stepPin = stepPin;
+  pinMode(stepPin, OUTPUT);
+  this->dirPin = dirPin;
+  pinMode(dirPin, OUTPUT);
+}
+
+void Motor::MakeStep(int Delay)
+{
+  digitalWrite(stepPin, HIGH);
+  delayMicroseconds(Delay);
+  digitalWrite(stepPin, LOW);
+  delayMicroseconds(Delay);
+}
+
+void Motor::SetDirection(Directions Direction)
+{
+  if (Direction == Clockwise)
+    digitalWrite(dirPin, HIGH);
+
+  if (Direction == CounterClockwise)
+    digitalWrite(dirPin, LOW);
+}
---
+++ ArduinoApp/lib/Motor/Motor.h
@@ -0,0 +1,22 @@
+#ifndef MOTOR_H_
+#define MOTOR_H_
+
+enum Directions
+{
+    Clockwise,
+    CounterClockwise
+};
+
+class Motor
+{
+
+    int stepPin;
+    int dirPin;
+
+public:
+    Motor(int stepPin, int dirPin);
+    void SetDirection(Directions Direction);
+    void MakeStep(int Delay);
+};
+
+#endif
